8e71b7cc798ec68b1528148a0d2dab58476fbef6
AlanDorz <JonyTest007@gmail.com>
2020-03-17 16:53:53 +0300

    small changes

--- javascript/main.js
+++ javascript/main.js
@@ -4,15 +4,10 @@ let apple = document.querySelector('.img_apple')
 
 let appleScale = anime({
     targets: apple,
-    // width: ['46vh', '90vh'],
     scale: 5,
     easing: 'linear',
-    // top: '300vh',
     autoplay: false
 })
-
-// let appleBtn = document.querySelector('.img_apple')
-// appleBtn.onclick = appleScale.play
 
 // ---magicScroll---
 
@@ -29,21 +24,3 @@ new ScrollMagic.Scene({
 .on('progress', function(e) {
     appleScale.seek(appleScale.duration * e.progress)
 })
-
-// let controller = new ScrollMagic.Controller({
-//     globalSceneOptions: {
-//         triggerHook: 'onEnter',
-//     }
-// })
-// let sectionScene = document.querySelector('div')
-
-// let appleScene = new ScrollMagic.Scene({
-//     triggerElement: sectionScene,
-//     duration: 500
-//     // duration: sectionScene.getBoundingClientRect().height
-// })
-
-// .setPin('.img_apple')
-// .addTo(controller)
-
-// // appleScene.oninput = e => {appleScale.seek(appleScale.duration * e.progress)}
